65cb25da6e9d865a7eec47924bdbb98f6867e7d6
Imesh Ruchira <116550333+98ruchira@users.noreply.github.com>
2023-12-11 15:50:44 +0530

    Update ec2.tf
    change associate part

--- AWS-EC2-secure/ec2.tf
+++ AWS-EC2-secure/ec2.tf
@@ -1,35 +1,46 @@
+resource "aws_key_pair" "test-pc-key" {
+  key_name   = var.key_pair_name
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_security_group" "RuchiraSG" {
   name        = "RuchiraSG"
   description = "Allow inbound SSH and HTTP traffic"
   vpc_id      = aws_vpc.Ruchira-VPC.id
-  tags        = var.security_group_tags
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.ssh_cidr_blocks
+    cidr_blocks = var.ssh_ingress_cidr_blocks
   }
 }
 
 resource "aws_instance" "Ruchira-EC2-Public" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = aws_subnet.Ruchira-Public-Subnet.id
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  associate_public_ip_address = true
+  subnet_id              = aws_subnet.Ruchira-Public-Subnet.id
   vpc_security_group_ids = [aws_security_group.RuchiraSG.id]
+  
 
-  tags = var.public_instance_tags
+  tags = {
+    Name = var.ec2_public_instance_name
+  }
 }
 
+
 resource "aws_instance" "Ruchira-EC2-Private" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = aws_subnet.Ruchira-Private-Subnet.id
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name  
+  subnet_id              = aws_subnet.Ruchira-Private-Subnet.id
   vpc_security_group_ids = [aws_security_group.RuchiraSG.id]
 
-  tags = var.private_instance_tags
+  tags = {
+    Name = var.ec2_private_instance_name
+  }
 }
 
 
