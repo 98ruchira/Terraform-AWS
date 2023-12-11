0d5182d97e37980b74a169b535ce74fa027c9fcc
Imesh Ruchira <116550333+98ruchira@users.noreply.github.com>
2023-12-11 15:51:16 +0530

    Update vpc.tf
    change associate part

--- AWS-EC2-secure/vpc.tf
+++ AWS-EC2-secure/vpc.tf
@@ -1,50 +1,85 @@
 resource "aws_vpc" "Ruchira-VPC" {
   cidr_block = var.vpc_cidr_block
-  tags       = var.vpc_tags
+  enable_dns_hostnames = true
+
+  tags       = {
+    Name = var.vpc_name
+  }
 }
 
 resource "aws_subnet" "Ruchira-Public-Subnet" {
   vpc_id     = aws_vpc.Ruchira-VPC.id
   cidr_block = var.public_subnet_cidr_block
-  tags       = var.public_subnet_tags
+
+  tags       = {
+    Name = var.public_subnet_name
+  }
+
 }
+
+resource "aws_route_table" "public_subnet_route_table" {
+    vpc_id = aws_vpc.Ruchira-VPC.id
+}
+
 
 resource "aws_subnet" "Ruchira-Private-Subnet" {
   vpc_id     = aws_vpc.Ruchira-VPC.id
   cidr_block = var.private_subnet_cidr_block
-  tags       = var.private_subnet_tags
+  
+  tags       = {
+    Name = var.private_subnet_name
+  }
+
+}
+
+resource "aws_route_table_association" "private_subnet_route_table_association" {
+  subnet_id = aws_subnet.Ruchira-Private-Subnet.id
+  route_table_id = aws_route_table.Ruchira_private_route_table.id
 }
 
 resource "aws_internet_gateway" "Ruchira-igw" {
   vpc_id = aws_vpc.Ruchira-VPC.id
-  tags   = var.igw_tags
+  
+  tags   = {
+    Name = var.internet_gateway_name
+  }
 }
 
-resource "aws_route_table" "Public-RT" {
+resource "aws_route_table" "Ruchira_Public_route_table" {
   vpc_id = aws_vpc.Ruchira-VPC.id
-  tags   = var.public_rt_tags
+  
+  tags   = {
+    Name = "Ruchira_Public_route_table"
+  }
 
-  route {
+    route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.Ruchira-igw.id
   }
 }
 
-resource "aws_route_table" "private_route_table" {
+resource "aws_route_table" "Ruchira_private_route_table" {
   vpc_id = aws_vpc.Ruchira-VPC.id
-  tags   = var.private_rt_tags
+  tags   = {
+    Name = "Ruchira_private_route_table"
+  }
 }
 
 resource "aws_eip" "Ruchira-eip" {
   domain = "vpc"
-  tags   = var.eip_tags
+  instance = aws_instance.Ruchira-EC2-Public.id
+  tags   = {
+    Name = "Ruchira-EIP"
+  }
 }
 
 resource "aws_nat_gateway" "Ruchira-NGW" {
   allocation_id = aws_eip.Ruchira-eip.id
   subnet_id     = aws_subnet.Ruchira-Public-Subnet.id
 
-  tags = var.nat_gateway_tags
+  tags = {
+    Name = "Ruchira-NAT-Gateway"
+  }
 
   depends_on = [aws_internet_gateway.Ruchira-igw]
 }
